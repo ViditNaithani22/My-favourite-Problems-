8dcaa973386e686ce5048543d5917b12f6194a87
Vidit Naithani <102232954+ViditNaithani22@users.noreply.github.com>
2022-12-29 13:43:49 +0530

    Running Sum Of 1D Array problem

---
+++ my favourite problems/src/RunningSumOf_1D_Array/solution1.java
@@ -0,0 +1,35 @@
+package RunningSumOf_1D_Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class solution1 {
+    public static int[] runningSum(int[] nums) {
+        int[]  results = new int[nums.length];
+        results[0] = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            results[i] = nums[i]+results[i-1];
+        }
+        return results;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter length of input array:");
+        int length = scan.nextInt();
+        System.out.println("enter elements of input array:");
+        int[] input = new int[length];
+        for (int i = 0; i<length;i++){
+            input[i] = scan.nextInt();
+        }
+
+        System.out.println("input array: "+Arrays.toString(input));
+
+        int[] result = runningSum(input);
+
+        System.out.println("output array: "+Arrays.toString(result));
+
+
+    }
+}
---
+++ my favourite problems/src/RunningSumOf_1D_Array/solution2.java
@@ -0,0 +1,36 @@
+package RunningSumOf_1D_Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class solution2 {
+    public static int[] runningSum(int[] nums) {
+
+        for(int i=1;i<nums.length;i++){
+            nums[i] += nums[i-1];
+        }
+
+        return nums;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("enter length of input array:");
+        int length = scan.nextInt();
+        System.out.println("enter elements of input array:");
+        int[] input = new int[length];
+        for (int i = 0; i<length;i++){
+            input[i] = scan.nextInt();
+        }
+
+        System.out.println("input array: "+ Arrays.toString(input));
+
+        int[] result = runningSum(input);
+
+        System.out.println("output array: "+Arrays.toString(result));
+
+
+    }
+}
