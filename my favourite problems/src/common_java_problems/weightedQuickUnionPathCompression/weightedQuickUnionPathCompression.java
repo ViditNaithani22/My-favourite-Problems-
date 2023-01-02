2b9cdcbc611b7979b86014df746d816697f9fcc4
Vidit Naithani <102232954+ViditNaithani22@users.noreply.github.com>
2023-01-02 22:37:32 +0530

    weighted quick union correction path compression

---
+++ my favourite problems/src/common_java_problems/weightedQuickUnionPathCompression/weightedQuickUnionPathCompression.java
@@ -0,0 +1,90 @@
+package common_java_problems.weightedQuickUnionPathCompression;
+
+import common_java_problems.weightedQuickUnion.weightedQuickUnion;
+
+public class weightedQuickUnionPathCompression {
+    int[] id;
+    int[] sz;
+    int count;
+
+
+    public weightedQuickUnionPathCompression(int n) {
+        count = n;
+        id = new int[n];
+        sz = new int[n];
+        for (int i = 0; i < n; i ++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public int getId(int i) {
+        return id[i];
+    }
+
+    int root(int i){
+        while (i != id[i]){
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+
+    boolean connected(int p, int q){
+        return root(p) == root(q);
+
+    }
+
+
+    void union(int p, int q){
+        int i = root(p);
+        int j = root(q);
+        if (i == j) return;
+
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        }
+        else if(sz[i] > sz[j]){
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+        else {
+            if(i>j){
+                id[j] = i;
+                sz[i] += sz[j];
+            }
+            else {
+                id[i] = j;
+                sz[j] += sz[i];
+            }
+        }
+
+        count --;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        weightedQuickUnionPathCompression qf = new weightedQuickUnionPathCompression(10);
+        qf.union(4,3);
+        qf.union(3,8);
+        qf.union(9,4);
+
+        System.out.println(qf.connected(9,8));
+        System.out.println(qf.connected(5,0));
+        System.out.println(qf.getId(4));
+        System.out.println(qf.getId(8));
+        System.out.println(qf.getId(3));
+        System.out.println(qf.getId(9));
+
+        System.out.println("number of components: "+ qf.count);
+
+    }
+
+}
